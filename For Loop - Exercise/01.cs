3391e4503954babb2ffbc183cac1c3ad0b609bb6
monikaez <86738350+monikaez@users.noreply.github.com>
2023-12-30 19:02:28 +0200

    03

---
+++ For Loop - Exercise/01.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace _01._Numbers_Ending_in_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 7; number <= 997; number++)
+            {
+                if(number % 10 == 7)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
